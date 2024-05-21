55020574e86e2716a6325b7f8e365c2a8f24e773
Saroj Padhan <40264980+saroj8455@users.noreply.github.com>
2024-05-21 12:50:07 +0530

    prime configuration done with button and flex componet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ConfigService } from './services/config.service';
+import { FlexiconsComponent } from './components/flexicons/flexicons.component';
+import { PrimeconfigModule } from './primeconfig/primeconfig.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FlexiconsComponent, PrimeconfigModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
