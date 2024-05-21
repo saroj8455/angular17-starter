db522c947b4e7c67551a65e79d445beb0152b665
Saroj Padhan <40264980+saroj8455@users.noreply.github.com>
2024-05-21 12:32:02 +0530

    able to make http api call and show in the network tab without ssr

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ConfigService } from './services/config.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'clientapi';
+
+  constructor(private configService: ConfigService) {}
+
+  ngOnInit(): void {
+    this.configService.loadProductsFromFakeApi().subscribe((products) => {
+      console.log(products);
+    });
+  }
 }
